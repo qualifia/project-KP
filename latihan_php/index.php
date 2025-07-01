bb1f0b7316669ec9e4e5994a215abe0e7aa1fc5d
lifiaa <112743090+qualifia@users.noreply.github.com>
2025-07-01 12:51:34 +0700

    Add files via upload

---
+++ latihan_php/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Utama</title>
+    
+</head>
+<body>
+    <?php
+        echo "<h1>Selamat datang di Yakes!</body>h1>";
+    ?>
+</body>
+</html>
