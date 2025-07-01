8dff7fba684bf00ae1dbdb7f5d3e3091b6f42499
lifiaa298 <quaaxy@gmail.com>
2025-07-01 14:21:32 +0700

    Create admin.php

---
+++ latihan_php/admin.php
@@ -0,0 +1,35 @@
+<?php
+include 'db.php';
+
+$query = "SELECT * FROM messages ORDER BY created_at DESC";
+$result = mysqli_query($conn, $query);
+?>
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pesan Masuk</title>
+</head>
+<body>
+    <h1>Pesan Masuk</h1>
+    <table border="1" cellpadding="10" cellspacing="0">
+        <tr>
+            <th>ID</th>
+            <th>Nama</th>
+            <th>Email</th>
+            <th>Pesan</th>
+            <th>Tanggal</th>
+        </tr>
+        <?php while ($row = mysqli_fetch_assoc($result)): ?>
+        <tr>
+            <td><?php echo $row['id']; ?></td>
+            <td><?php echo $row['name']; ?></td>
+            <td><?php echo $row['email']; ?></td>
+            <td><?php echo $row['message']; ?></td>
+            <td><?php echo $row['created_at']; ?></td>
+        </tr>
+        <?php endwhile; ?>
+    </table>
+</body>
+</html>
