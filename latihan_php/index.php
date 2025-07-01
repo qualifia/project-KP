b835a6980f67765f715483b15bc1adbccd3340e5
lifiaa298 <quaaxy@gmail.com>
2025-07-01 13:27:39 +0700

    Update index.php

--- latihan_php/index.php
+++ latihan_php/index.php
@@ -4,11 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Halaman Utama</title>
-    
+    <link rel="stylesheet" href="style.css"> 
 </head>
 <body>
-    <?php
-        echo "<h1>Selamat datang di Yakes!</body>h1>";
-    ?>
+    <nav>
+        <ul>
+            
+            <li><a href="about.php">Tentang Kami</a></li>
+            <li><a href="contact.php">Kontak Kami</a></li>
+            <li><a href="login.php">Login</a></li>
+        </ul>
+    </nav>
+    <section>
+        <h1>Selamat datang di Yakes!</h1>
+        <p>Website ini digunakan untuk blablabla</p>
+    </section>
 </body>
 </html>
